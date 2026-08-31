174454a281c7be62e4f362e1c0b53bd2197f7b93
ankitabhojwani <ankitabhojwani18@gmail.com>
2026-08-31 17:16:27 +0530

    Time: 4 ms (99.1%), Space: 47.5 MB (93.36%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String str,int left , int right){
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int left =0; 
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                if(isPalindrome(s,left,right-1) || isPalindrome(s, left+1,right) ) {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
